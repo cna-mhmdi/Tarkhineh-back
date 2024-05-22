2452af5de7d7659ce52ce1c6c82c3ba7491c1a1a
cna <cna.mhmdi0435@gmail.com>
2024-05-22 14:43:24 +0330

    user api added

--- api/user_test.go
+++ api/user_test.go
@@ -8,6 +8,7 @@ import (
 	mockdb "github.com/cna-mhmdi/Tarkhineh-back/db/mock"
 	db "github.com/cna-mhmdi/Tarkhineh-back/db/sqlc"
 	"github.com/cna-mhmdi/Tarkhineh-back/util"
+	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
@@ -15,6 +16,117 @@ import (
 	"net/http/httptest"
 	"testing"
 )
+
+func TestCreateUser(t *testing.T) {
+	user := randomUser()
+
+	testCase := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"username":      user.Username,
+				"password_hash": user.PasswordHash,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+
+				arg := db.CreateUserParams{
+					Username:     user.Username,
+					PasswordHash: user.PasswordHash,
+				}
+
+				store.EXPECT().
+					CreateUser(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(user, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchAccount(t, recorder.Body, user)
+			},
+		},
+		{
+			name: "InternalError",
+			body: gin.H{
+				"username":      user.Username,
+				"password_hash": user.PasswordHash,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+
+				store.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.User{}, sql.ErrConnDone)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "UserNameDuplicated",
+			body: gin.H{
+				"username":      "sina1",
+				"password_hash": user.PasswordHash,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+
+				arg := db.CreateUserParams{
+					Username:     "sina1",
+					PasswordHash: user.PasswordHash,
+				}
+
+				store.EXPECT().
+					CreateUser(gomock.Any(), arg).
+					Times(1).
+					Return(db.User{}, sql.ErrConnDone)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "UsernameRequired",
+			body: gin.H{
+				"username":      nil,
+				"password_hash": user.PasswordHash,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCase {
+		tc := testCase[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/user"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+}
 
 func TestGetUser(t *testing.T) {
 	user := randomUser()
